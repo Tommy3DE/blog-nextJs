0e68caf8bb3ba08d41541d2ac8ade3f2467bf210
Patryk Kowalski <kowalus23@gmail.com>
2023-02-22 17:47:31 +0100

    fixes

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -2,15 +2,15 @@ import Link from 'next/link'
 import { PostMetadata } from './postMetadata'
 
 const PostPreview = (post: PostMetadata) => {
-  return (
-  <div className='border border-slate-300 p-4 rounded-md shadow-md bg-white'>
-    <p className='text-sm text-slate-400'>{post.date}</p>
-    <Link href={`/posts/${post.id}`}>
-      <h2 className='text-violet-600 hover:underline  mb-4'>{post.title}</h2>
-    </Link>
-    <p className='text-slate-700'>{post.subtext}</p>
-  </div>
-  )
+    return (
+        <div className="border border-slate-300 p-4 rounded-md shadow-md bg-white">
+            <p className="text-sm text-slate-400">{post.date}</p>
+            <Link href={`/posts/${post.id}`}>
+                <h2 className="text-violet-600 hover:underline  mb-4">{post.title}</h2>
+            </Link>
+            <p className="text-slate-700">{post.subtext}</p>
+        </div>
+    )
 }
 
 export default PostPreview
