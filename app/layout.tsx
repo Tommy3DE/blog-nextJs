566bf13ee03c9c73c6dca43f12de838d8b7d2eab
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-02-22 11:01:39 +0100

    stylowanie strony artykulow i fixy tailwind na glownej

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import "../styles/globals.css";
 export default function RootLayout({
@@ -11,14 +12,17 @@ export default function RootLayout({
       <body>
         <div className="mx-auto max-w-2xl px-6">
           <header className="text-center bg-slate-800 p-8 my-6 rounded-md">
+            <Image src="/favicon.ico" alt="logo" width={200} height={200} className='mx-auto'/>
             <Link href="/">
-              <h1 className="text-3xl text-white font-bold">Nowy Blog</h1>
+              <h1 className="text-2xl text-white font-bold mt-6">Nowy Blog</h1>
             </Link>
             <p className="text-slate-300">Witaj na moim blogu</p>
           </header>
           {children}
-          <footer className="border-t text-center text-slate-500 border-slate-400 mt-6 py-6">
-            <h3>2023 ®</h3>
+          <footer>
+            <div  className="border-t text-center text-slate-500 border-slate-400 py-6 mt-12">
+              <h3>Tomasz Kuniewicz 2023 ®</h3>
+            </div>
           </footer>
         </div>
       </body>
