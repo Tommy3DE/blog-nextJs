e2392c7692a57c1bad0fc25048beae15dfd7a61e
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-02-22 10:35:40 +0100

    stylowanie strony głównej i bardziej czytelne substrony artykulow

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-
+import "../styles/globals.css";
 export default function RootLayout({
   children,
 }: {
@@ -9,13 +9,18 @@ export default function RootLayout({
     <html>
       <head />
       <body>
-        <header>
-          <Link href="/">
-            <h1>Nowy Blog</h1>
-          </Link>
-        </header>
-        {children}
-        <footer>2023 ®</footer>
+        <div className="mx-auto max-w-2xl px-6">
+          <header className="text-center bg-slate-800 p-8 my-6 rounded-md">
+            <Link href="/">
+              <h1 className="text-3xl text-white font-bold">Nowy Blog</h1>
+            </Link>
+            <p className="text-slate-300">Witaj na moim blogu</p>
+          </header>
+          {children}
+          <footer className="border-t text-center text-slate-500 border-slate-400 mt-6 py-6">
+            <h3>2023 ®</h3>
+          </footer>
+        </div>
       </body>
     </html>
   );
